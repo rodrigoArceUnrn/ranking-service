07378e9fefcfd6d5670e61208a2e63cd0da2f7c5
Rodrigo <arcerodrigo3@gmail.com>
2022-10-23 16:44:35 -0300

    RankingService create

--- src/main/java/ar/edu/unrn/productservice/RankingServiceApplication.java
+++ src/main/java/ar/edu/unrn/productservice/RankingServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class RankingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(RankingServiceApplication.class, args);
     }
 
     @Bean
